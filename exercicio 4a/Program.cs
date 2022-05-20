7dddf2c1034059aa1eda9b2f991f83e9ab1fed8c
ana.silva69 <ana.silva69@ITELABD10>
2022-05-19 21:11:44 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio 4a/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace exercicio_4a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+            int valor;
+
+            Console.WriteLine("Informe o valor");
+            valor = Convert.ToInt32(Console.ReadLine());
+            int[] vetorint = new int[valor];
+            int[] vetorcopia = new int[valor];
+            int[] vetorcontarrepet = new int[valor];
+            do
+            {
+                Console.WriteLine("Informe um valor");
+                vetorint[cont] = Convert.ToInt32(Console.ReadLine());
+                vetorcopia[cont] = vetorint[cont];
+                cont++;
+
+            } while (cont < vetorint.Length);
+
+            cont = 0;
+            int cont1 = 0;
+            do
+            {
+                do
+                {
+                    if (cont != cont1)
+                    {
+                        if (vetorint[cont1] == vetorcopia[cont])
+                        {
+                            vetorcontarrepet[cont1]++;
+                        }
+                    }
+                   
+                    cont++;
+
+                } while (cont < vetorint.Length);
+                cont = 0;
+                cont1++;
+
+            }while (cont1 < vetorint.Length);
+            cont1 = 0;
+            do
+            {
+                Console.WriteLine(vetorcontarrepet[cont1]);
+                cont1++;
+            } while (cont1 < vetorcontarrepet.Length);
+
+        }
+    }
+}
